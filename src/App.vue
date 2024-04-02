3324a3ca969cc88aa761548de4bc807d9e5de352
Shist <shist.pupust@mail.ru>
2024-04-02 04:15:04 +0300

    Added block with posts (with title and body) using v-for

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
   <div>
-    <div>
-      <button @click="addLike">Like</button>
-      <button @click="addDislike">Dislike</button>
-    </div>
-    <div>
-      Кол-во лайков: <strong>{{ likes }}</strong>
-    </div>
-    <div>
-      Кол-во дизлайков: <strong>{{ dislikes }}</strong>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong> {{ post.title }}</div>
+      <div><strong>Описание:</strong> {{ post.body }}</div>
     </div>
   </div>
 </template>
@@ -17,19 +11,28 @@
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 5,
+      posts: [
+        { id: 1, title: "JavaScript 1", body: "Описание поста 1" },
+        { id: 2, title: "JavaScript 2", body: "Описание поста 2" },
+        { id: 3, title: "JavaScript 3", body: "Описание поста 3" },
+        { id: 4, title: "JavaScript 4", body: "Описание поста 4" },
+      ],
     };
   },
-  methods: {
-    addLike() {
-      this.likes += 1;
-    },
-    addDislike() {
-      this.dislikes += 1;
-    },
-  },
+  methods: {},
 };
 </script>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+</style>
