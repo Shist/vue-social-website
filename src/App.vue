3572b4c440e3153905e9d02cbae5e6846aa1d703
Shist <shist.pupust@mail.ru>
2024-04-02 04:39:58 +0300

    Added creation of posts by pushing them to an array

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <form>
+    <form @submit.prevent>
       <h4>Создание поста</h4>
       <input
         v-bind:value="title"
@@ -40,7 +40,16 @@ export default {
     };
   },
   methods: {
-    createPost() {},
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = "";
+      this.body = "";
+    },
   },
 };
 </script>
