1237e70d00ef580e3d67e143d942cbda9cba0385
Shist <shist.pupust@mail.ru>
2024-04-03 11:29:56 +0300

    Added modal dialog for creating new posts

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="app">
-    <post-form @create="createPost" />
+    <h1>Страница с постами</h1>
+    <my-button
+      class="btn_show-dialog"
+      @click="showDialog"
+    >
+      Создать пост
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-dialog>
     <post-list :posts="posts" @remove="removePost"/>
   </div>
 </template>
@@ -22,14 +31,19 @@ export default {
         { id: 3, title: "JavaScript 3", body: "Описание поста 3" },
         { id: 4, title: "JavaScript 4", body: "Описание поста 4" },
       ],
+      dialogVisible: false
     };
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter(nextPost => nextPost.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true;
     }
   },
 };
@@ -43,5 +57,8 @@ export default {
 }
 .app {
   padding: 20px;
+}
+.btn_show-dialog {
+  margin: 15px 0;
 }
 </style>
